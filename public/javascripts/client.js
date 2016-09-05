9a2339e1af2b61124d152de66ff70b54a2f62e1c
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-09-05 16:18:36 -0400

    Added angular module, used ng-repeat to show a collection of bars as buttons

---
+++ public/javascripts/client.js
@@ -0,0 +1,18 @@
+angular.module("whatsOnTap", [])
+
+.controller("controller", function() {
+	this.bars = [
+					{ name: "Bar 1", neighborhood: "This neighborhood" },
+					{ name: "Bar 2", neighborhood: "This neighborhood" },
+					{ name: "Bar 3", neighborhood: "This neighborhood" },
+					{ name: "Bar 4", neighborhood: "This neighborhood" },
+					{ name: "Bar 5", neighborhood: "This neighborhood" },
+					{ name: "Bar 6", neighborhood: "This neighborhood" },
+					{ name: "Bar 7", neighborhood: "This neighborhood" },
+					{ name: "Bar 8", neighborhood: "This neighborhood" },
+					{ name: "Bar 9", neighborhood: "This neighborhood" },
+					{ name: "Bar 10", neighborhood: "This neighborhood" },
+					{ name: "Bar 11", neighborhood: "This neighborhood" },
+					{ name: "Bar 12", neighborhood: "This neighborhood" },
+				]
+})
