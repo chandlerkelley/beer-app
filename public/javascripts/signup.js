1a11e3e350b88daf94eca7249014fe3b932a46b9
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-09-09 10:59:02 -0400

    Some form validation on the signup form works

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -5,16 +5,23 @@ angular.module("whatsOnTap")
 		<form name="form" ng-submit="$ctrl.signup(form)" novalidate>
 			<div class="form-part">
 				<label class="form-label">Email</label>
-				<input type="email" name="email" ng-model="$ctrl.user.email">
+				<input type="email" name="email" ng-model="$ctrl.user.email" ng-blur="$ctrl.emailBlur = true" required mongoose-error>
 			</div>
+			<p ng-show="form.email.$error.required && $ctrl.emailBlur">What's your email?</p>
+			<p ng-show="form.email.$error.email && $ctrl.emailBlur">That doesn't look like a valid email</p>
+			
 			<div class="form-part">
 				<label class="form-label">Password</label>
-				<input type="password" name="password" ng-model="$ctrl.user.password">
+				<input type="password" name="password" ng-model="$ctrl.user.password" ng-blur="$ctrl.passwordBlur = true" ng-minlength="6" required>
 			</div>
+			<p ng-show="(form.password.$error.required || form.password.$error.minlength) && $ctrl.passwordBlur">Password must contain at least 6 characters</p>
+			
 			<div class="form-part">
 				<label>Confirm Password</label>
-				<input type="password" name="password-check" ng-model="$ctrl.passwordCheck">
+				<input type="password" name="passwordCheck" ng-model="$ctrl.user.passwordCheck" ng-blur="$ctrl.passwordCheckBlur = true" match="$ctrl.user.password" required>
 			</div>
+			<p ng-show="form.passwordCheck.$error.match && $ctrl.passwordCheckBlur">Passwords must match</p>
+
 			<button class="btn form-button" type="submit">Register</button>
 		</form>
 	</main>
@@ -22,6 +29,8 @@ angular.module("whatsOnTap")
 	controller: function(Auth, $state, toastr) {
 
 		this.signup = function(form) {
+			this.submitted = true;
+
 			return Auth.createUser({
 				email: this.user.email,
 				password: this.user.password
